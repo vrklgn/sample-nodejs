2ff166226c216c550146b6953eb4aeae3009251d
Robert Levelind <robert.levelind@tink.com>
2024-08-27 13:26:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,11 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-var LoremIpsum = require('lorem-ipsum').LoremIpsum;
 
-var lorem = new LoremIpsum({
-  sentencesPerParagraph: {
-    max: 8,
-    min: 4
-  },
-  wordsPerSentence: {
-    max: 16,
-    min: 4
-  }
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
 });
-
-app.get('/', (req, res) => res.send(lorem.generateParagraphs(7)))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
